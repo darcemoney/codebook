adbe4891ca07902877fe269e2a462710d254c1a3
Darcy Hayes <darcemoney1@gmail.com>
2019-01-03 16:59:58 -0500

    update a-were

--- platformer/game.js
+++ platformer/game.js
@@ -1,19 +1,26 @@
-let p, k
+let p, k, plats
 let keys = 'RIGHT,LEFT,UP,DOWN,SPACE,W,A,S,D'
 
 
 const preload = function() {
     this.load.image('dg','./assets/plat-dg.png')
     this.load.image('pl','./assets/wow.png')
+    this.load.image('bgg','./assets/boxeeloxeeV2.png')
+    this.load.image('form','./assets/thecolorboxeeloxee.png')
 }
 
 const create = function() {
     this.add.image(0,0,'dg').setOrigin(0,0)
+    
+    plats = this.physics.add.staticGroup()
+    plats.create(200,300,'form')
+
     pl = this.physics.add.sprite(100,100,'pl')
     pl.setScale(2)
     pl.setCollideWorldBounds(true)
+    pl.setGravityY(1200)
 
-
+    this.physics.add.collider(pl,form)
 
     k = this.input.keyboard.addKeys(keys)
 }
@@ -28,7 +35,7 @@ else if (k.RIGHT.isDown) {
     pl.setVelocityX(400)
 }
 
-if (k.UP.isDown) {
+if (k.UP.isDown && pl.body.onFloor()) {
     pl.setVelocityY(-300)
 }
 }
@@ -37,14 +44,9 @@ let config = {
     height: 384,
     pixelArt: true,
     scene: { preload, create, update },
-    physics: {
-        default: 'arcade',
-        arcade: { 
-            gravity:{ y: 300},
-            debug: false
-        }
-    },
-}
+    physics: { default: 'arcade'},
+    }
+
 
 
 new Phaser.Game(config)
